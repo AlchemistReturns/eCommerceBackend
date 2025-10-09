2c2b09d97286cedbade90201efb8ae9344903d50
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2025-10-09 21:36:42 +0600

    Code the user controller - return all users and return user by id

---
+++ src/main/java/com/abrar/store/controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.abrar.store.controllers;
+
+import com.abrar.store.dtos.UserDto;
+import com.abrar.store.entities.User;
+import com.abrar.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        User user =  userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        UserDto userDto =  new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
+    }
+
+
+}
--- src/main/java/com/abrar/store/repositories/UserRepository.java
+++ src/main/java/com/abrar/store/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package com.abrar.store.repositories;
 
+import com.abrar.store.dtos.UserDto;
 import com.abrar.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
