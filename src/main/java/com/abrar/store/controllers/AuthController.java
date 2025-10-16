efcba902652bd7417d953ea12fcb81e7d95a730a
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2025-10-16 13:07:58 +0600

    Implement AuthenticationManager with DaoAuthenticationProvider

--- src/main/java/com/abrar/store/controllers/AuthController.java
+++ src/main/java/com/abrar/store/controllers/AuthController.java
@@ -1,34 +1,29 @@
 package com.abrar.store.controllers;
 
 import com.abrar.store.dtos.UserLoginRequest;
-import com.abrar.store.entities.User;
-import com.abrar.store.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody UserLoginRequest request) {
-        User user = userRepository.findByEmail(request.getEmail());
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-        if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.OK).build();
-        }
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
